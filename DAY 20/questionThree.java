2cbd2a74b9c20df7d0606024db5321b36e3ffa75
anshumanp107-gif <anshumanp107@gmail.com>
2026-06-29 23:41:12 +0530

    DAY 20 WORK

---
+++ DAY 20/questionThree.java
@@ -0,0 +1,18 @@
+public class questionThree {
+    public static void main(String[] args) {
+
+        int[][] a={
+                {1,2,3},
+                {4,5,6},
+                {7,8,9}
+        };
+
+        for(int i=0;i<a.length;i++){
+            int sum=0;
+            for(int j=0;j<a[0].length;j++){
+                sum+=a[i][j];
+            }
+            System.out.println("Row "+(i+1)+" Sum = "+sum);
+        }
+    }
+}
